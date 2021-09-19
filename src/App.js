5df91c871bddaeca6977a672f693a2c9d342644e
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-09-20 00:46:59 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ const App = () => {
   const APP_ID = '703c21fa';
   const APP_KEY = '806b451eaf2876794c46545f4032f444';
 
-  const [counter,setCounter] = useState(0);
-
 
   useEffect(() =>{
-    console.log('Effect has been run');
 
   });
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  }
 
   return (
     <div className="App">
