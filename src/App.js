c5862ce4d0bdd9a91dcbc925552f5510ffe5e4c8
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-09-20 00:06:27 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,12 +6,19 @@ const App = () => {
   const APP_ID = '703c21fa';
   const APP_KEY = '806b451eaf2876794c46545f4032f444';
 
+  const [counter,setCounter] = useState(0);
+
+
+  useEffect(() =>{
+    console.log('Effect has been run');
+
+  });
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button className="search-button" type="submit">SEARCH</button>
+        <button className="search-button" type="submit">{counter}SEARCH</button>
       </form>
     </div>
   );
